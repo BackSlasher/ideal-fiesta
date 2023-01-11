fc1d9f3353ca36a8e893684638c0ea0c9d9e588d
Nitzan Raz <nitz.raz@gmail.com>
2023-01-11 19:49:12 +0200

    black

--- analyzer.py
+++ analyzer.py
@@ -4,26 +4,28 @@ import datetime
 import argparse
 import csv
 
+
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("filename", type=argparse.FileType('r'))
-    parser.add_argument("-b","--hours-back", type=int, default=1)
+    parser.add_argument("filename", type=argparse.FileType("r"))
+    parser.add_argument("-b", "--hours-back", type=int, default=1)
     return parser.parse_args()
 
+
 def analyze_data(file, hours_back):
-    reader = csv.DictReader(file, fieldnames=['time','memory'])
+    reader = csv.DictReader(file, fieldnames=["time", "memory"])
     rows = [row for row in reader]
     for row in rows:
-        row['time'] = datetime.datetime.fromtimestamp(float(row['time']))
-        row['memory'] = int(row['memory'])
+        row["time"] = datetime.datetime.fromtimestamp(float(row["time"]))
+        row["memory"] = int(row["memory"])
 
     # filter
     now = datetime.datetime.now()
     allowed = datetime.timedelta(hours=hours_back)
-    rows = [row for row in rows if (now - row['time']) < allowed]
+    rows = [row for row in rows if (now - row["time"]) < allowed]
 
     # max
-    max_mem = max([row['memory'] for row in rows])
+    max_mem = max([row["memory"] for row in rows])
 
     # magic
     recommended_mem = int(max_mem * 1.15)
@@ -36,6 +38,5 @@ def main():
     analyze_data(args.filename, args.hours_back)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
-
--- sampler/main.py
+++ sampler/main.py
@@ -5,16 +5,17 @@ import time
 from datetime import datetime
 import requests
 
+
 def sample_memory():
     try:
-        dat = requests.get('http://server:9000/mem_info', timeout=3)
+        dat = requests.get("http://server:9000/mem_info", timeout=3)
         mem = int(dat.content.strip())
         print(mem)
     except Exception as e:
         mem = -1
         print(e)
 
-    with open("/output/data.csv", 'a') as f:
+    with open("/output/data.csv", "a") as f:
         print(f"{datetime.now().timestamp()},{mem}", file=f)
 
 
@@ -25,5 +26,6 @@ def main():
         # tried fancier things, didn't work. This is enough
         time.sleep(5)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
